ebecc26c68cad1cb766c96e8b26b5e6a420bd814
Ngo Xuan Hai <hngoxuantg@gmail.com>
2025-08-07 22:55:17 +0700

    Update

--- Project.Common/Models/Responses/ApiResponse.cs
+++ Project.Common/Models/Responses/ApiResponse.cs
@@ -18,7 +18,7 @@
     {
         public bool Success { get; set; }
         public string? Message { get; set; }
-        public DateTime? Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }
         public ApiResponse() { }
         public ApiResponse(bool success, string? message)
         {
